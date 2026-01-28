c8b38f38c72ebb55ec8a708ef9c57b4286d7c221
Fanush <fgabriel.mihailescu@gmail.com>
2026-01-28 13:25:22 +0200

    feat: added the control options

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 console.log('hello there');
+
+
+const difficultyInput = document.querySelector('#difficulty-fieldset')! as HTMLFieldSetElement;
+const modeInput = document.querySelector('#mode-fieldset')! as HTMLFieldSetElement;
+
+difficultyInput.addEventListener('change', (e) => {
+    const target = e.target as HTMLInputElement;
+    console.log(target.value);
+})
+modeInput.addEventListener('change', (e) => {
+    const target = e.target as HTMLInputElement;
+    console.log(target.value);
+})
